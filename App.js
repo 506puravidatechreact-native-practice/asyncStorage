0bc52ea68956e3280dc41736e7dc9542af432735
Manuel Herrera Mesén <mob.manu.herrera.mesen@gmail.com>
2021-07-02 18:07:46 -0600

    latest changes related to how to show data storage and how it was remove it

--- App.js
+++ App.js
@@ -1,10 +1,70 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableHighlight,
+} from 'react-native';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const App = () => {
+  const [inputTextValue, saveInputTextValue] = useState('');
+  const [nameStorage, saveNameStorage] = useState('');
+
+  useEffect(() => {
+    getDataFromStorage();
+  }, []);
+
+  const saveData = async () => {
+    try {
+      await AsyncStorage.setItem('name', inputTextValue);
+      //console.log('saving ...  ' + inputTextValue);
+      saveNameStorage(inputTextValue);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getDataFromStorage = async () => {
+    try {
+      const name = await AsyncStorage.getItem('name');
+      //console.log('getting ...  ' + name);
+      saveNameStorage(name);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const removeData = async () => {
+    try {
+      await AsyncStorage.removeItem('name');
+      saveNameStorage('');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <View>
-      <Text style={styles.wrapperContent}>Async Storage</Text>
+    <View style={styles.wrapperContent}>
+      {nameStorage ? <Text> Hola: {nameStorage} </Text> : null}
+      <TextInput
+        style={styles.input}
+        placeholder={'Please write a name'}
+        onChangeText={text => saveInputTextValue(text)}
+      />
+
+      <Button title="Save" color="#333" onPress={() => saveData()} />
+
+      {nameStorage ? (
+        <TouchableHighlight
+          onPress={() => removeData()}
+          style={styles.btnRemove}>
+          <Text style={styles.text}>Remove Name &times;</Text>
+        </TouchableHighlight>
+      ) : null}
     </View>
   );
 };
@@ -12,8 +72,28 @@ const App = () => {
 const styles = StyleSheet.create({
   wrapperContent: {
     marginTop: 40,
-    fontSize: 32,
+    backgroundColor: '#FFF',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'white',
+    fontWeight: 'bold',
     textAlign: 'center',
+    textTransform: 'uppercase',
+    width: 300,
+  },
+  input: {
+    borderColor: '#666',
+    borderBottomWidth: 1,
+    width: 300,
+    height: 40,
+  },
+  btnRemove: {
+    backgroundColor: 'red',
+    marginTop: 30,
+    padding: 10,
   },
 });
 
